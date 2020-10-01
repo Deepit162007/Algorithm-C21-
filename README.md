56adae1e9385963b919a5b7afa9d7f3c79621f44
Deepit162007 <70428754+Deepit162007@users.noreply.github.com>
2020-10-01 16:05:06 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Algorithm-C21-
+# bounceOffAlgorithm
+Algorithm to bounce off two objects
---
+++ sketch.js
@@ -0,0 +1,42 @@
+var fixedRect, movingRect;
+
+function setup() {
+  createCanvas(1200,800);
+  fixedRect = createSprite(400, 100, 50, 80);
+  fixedRect.shapeColor = "green";
+  fixedRect.debug = true;
+  movingRect = createSprite(400, 800,80,30);
+  movingRect.shapeColor = "green";
+  movingRect.debug = true;
+
+  box1=createSprite(350,500,40,40);
+  box2=createSprite(200,100,40,40);
+  movingRect.velocityY = -5;
+  fixedRect.velocityY = +5;
+}
+
+function draw() {
+  background(0,0,0);  
+movingRect.y=mouseY;
+movingRect.x=mouseX;
+  if (isTouching(movingRect,box1)) {
+movingRect.shapeColor="yellow";
+box1.shapeColor="red";
+
+  }else{
+    movingRect.shapeColor="green";
+box1.shapeColor="green";
+  }
+  drawSprites();
+}
+
+function isTouching(object1,object2){
+  if (object1.x - object2.x < object2.width/2 + object1.width/2
+    && object2.x - object1.x < object2.width/2 + object1.width/2
+    &&object1.y - object2.y < object2.height/2 + object1.height/2
+    && object2.y - object1.y < object2.height/2 + object1.height/2) {
+  return true;
+}else {return false}
+
+
+}
